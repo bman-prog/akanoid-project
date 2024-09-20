24cee608cf855445f3e70f25bdfa43d913d3d4b4
bman-prog <bmankovski2028@communityschoolnaples.org>
2024-09-20 11:42:02 -0400

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -6,6 +6,9 @@ scene.onOverlapTile(SpriteKind.ball, assets.tile`myTile`, function (sprite, loca
     BounceBall(ball)
     info.changeScoreBy(1)
 })
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+	
+})
 function spawnBall () {
     ball = sprites.create(img`
         . . . . . . . . . . . . . . . . 
@@ -31,40 +34,20 @@ function spawnBall () {
 }
 function SpawnPlayer () {
     hero = sprites.create(img`
-        222222222222222222222222222222
-        222222222222222222222222222222
-        222222222222222222222222222222
-        222222222222222222222222222222
-        222222222222222222222222222222
-        222222222222222222222222222222
-        222222222222222222222222222222
-        222222222222222222222222222222
-        222222222222222222222222222222
-        222222222222222222222222222222
-        222222222222222222222222222222
-        222222222222222222222222222222
-        222222222222222222222222222222
-        222222222222222222222222222222
-        222222222222222222222222222222
-        222222222222222222222222222222
-        222222222222222222222222222222
-        22222222222222222222222222....
-        22222222222222222222222...2222
-        222222222222222222222..2222222
-        22222222222222222222.222222222
-        22222222222222222222.222222222
-        22222222222222222222.222222222
-        22222222222222222222.222222222
-        22222222222222222222.222222222
-        222222222222222222222..2222222
-        22222222222222222222222..22222
-        2222222222222222222222222..222
-        222222222222222222222222222.22
-        2222222222222222222222222222.2
+        2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+        2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+        2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+        2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+        2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
         `, SpriteKind.Player)
     hero.setPosition(84, 104)
     controller.moveSprite(hero, 300, controller.dx())
 }
+scene.onOverlapTile(SpriteKind.ball, assets.tile`myTile0`, function (sprite, location) {
+    tiles.setTileAt(location, assets.tile`myTile2`)
+    BounceBall(ball)
+    info.changeScoreBy(7)
+})
 function BounceBall (ballSprite: Sprite) {
     ballvx = randint(ballSpeed / 3, ballSpeed)
     ballvy = ballSprite.vy * -1
@@ -78,6 +61,17 @@ scene.onOverlapTile(SpriteKind.ball, assets.tile`myTile2`, function (sprite, loc
     BounceBall(ball)
     info.changeScoreBy(3)
 })
+scene.onOverlapTile(SpriteKind.ball, assets.tile`myTile1`, function (sprite, location) {
+    tiles.setTileAt(location, assets.tile`myTile0`)
+    BounceBall(ball)
+    info.changeScoreBy(10)
+})
+function ADVANCELEVEL () {
+    tiles.setCurrentTilemap(level_maps[level])
+    level += 1
+    spawnBall()
+    game.splash("Level " + level)
+}
 sprites.onOverlap(SpriteKind.Player, SpriteKind.ball, function (sprite, otherSprite) {
     BounceBall(otherSprite)
     otherSprite.y = sprite.top - 1
@@ -87,9 +81,26 @@ let ball: Sprite = null
 let ballSpeed = 0
 let ballvx = 0
 let ballvy = 0
-tiles.setCurrentTilemap(tilemap`level2`)
-SpawnPlayer()
+let level = 0
+let level_maps: tiles.TileMapData[] = []
+level_maps = [tilemap`level2`, tilemap`level9`, tilemap`level2`]
+let LevelScoreNeeded = [20, 50, 60]
+let totalscoreneeded = 0
+level = 0
 ballvy = 100
 ballvx = 100
-ballSpeed = 100
-spawnBall()
+ballSpeed = 1000
+info.setScore(0)
+ADVANCELEVEL()
+SpawnPlayer()
+game.onUpdate(function () {
+    if (true) {
+    	
+    }
+})
+game.onUpdate(function () {
+    totalscoreneeded += LevelScoreNeeded[level]
+    if (info.score() == totalscoreneeded) {
+        ADVANCELEVEL()
+    }
+})
